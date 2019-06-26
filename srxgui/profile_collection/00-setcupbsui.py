aed339d4456357b862e8f707de48a3bdab9e3968
whishei <sheila.whitman44@gmail.com>
2019-06-26 11:00:31 -0400

    Adding Profile = srxgui
    To load into ipython use the command:
    ipython --profile=srxgui

--- srxgui/profile_collection/00-setcupbsui.py
+++
@@ -1,9 +0,0 @@
-from bluesky import RunEngine
-import ophyd
-import databroker
-from ophyd.sim import det1, det2
-from bluesky.plans import count
-import numpy as np
-import matplotlib.pyplot as plt
-
-RE = RunEngine({})
